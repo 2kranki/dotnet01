1c18f2e721ef5a74b4501c9e46577ab5f652818c
bob <kranki@me.com>
2019-03-06 08:32:02 -0500

    Changed the docker build to use /usr/src/app which is more appropriate

--- console04/myDatabase/setupMSSQL1.sh
+++ console04/myDatabase/setupMSSQL1.sh
@@ -10,22 +10,27 @@ sudo docker pull mcr.microsoft.com/mssql/server:2017-latest
 # Run mssql1 the first time using host port 1401
 sudo docker run -e 'ACCEPT_EULA=Y' -e 'SA_PASSWORD=Passw0rd!' \
    -p 1401:1433 --name mssql1 \
-   -d mcr.microsoft.com/mssql/server:2017-latest
+   -d mcr.microsoft.com/mssql/server:2017-latest-ubuntu
+
+# Create /usr/src/app where we will put everything
+sudo docker exec -it mssql1 mkdir -p /usr/src
 
 # Copy the directory, ./dockerBuild/install, into mssql1 as /install
-sudo docker cp ./dockerBuild/rw mssql1:/
+sudo docker cp ./dockerBuild/app mssql1:/usr/src/
 
 # Install VI.
-sudo docker exec -it mssql1 "/rw/setup.sh"
+sudo docker exec -it mssql1 "/usr/src/app/setup.sh"
 
 # Install TSQLV4.
-sudo docker cp ~/git/TSQLV4.mod.sql.txt mssql1:/rw/
-sudo docker exec -it mssql1 "/rw/tsqlLoad.sh"
+if [-d ~/git] && [-f ~/git/TSQLV4.mod.sql.txt]; then
+	sudo docker cp ~/git/TSQLV4.mod.sql.txt mssql1:/usr/src/app/
+	sudo docker exec -it mssql1 "/usr/src/app/tsqlLoad.sh"
+fi
 
 # Install TeachSQL.
-sudo docker cp ~/git/create.sql.txt mssql1:/rw/
-sudo docker exec -it mssql1 "/rw/teachLoad.sh"
+sudo docker cp ~/git/create.sql.txt mssql1:/usr/src/app/
+sudo docker exec -it mssql1 "/usr/src/app/teachLoad.sh"
 
 # Install my sample SQL.
-sudo docker exec -it mssql1 "/rw/load.sh"
+sudo docker exec -it mssql1 "/usr/src/app/load.sh"
 
